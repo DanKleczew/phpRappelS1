05b762de44f7ccd40fdbda8f64d188d9f8fe3525
pixelexe <rerezheng@gmail.com>
2024-01-20 16:45:21 +0100

    fix personne

--- src/Model/Clients.php
+++ src/Model/Clients.php
@@ -19,18 +19,9 @@ class Clients
         $this->connexion = $connexionBDD;
     }
 
-    public function addNewUser($user){
-        $this->name = $user->getname();
-        $this->firstName = $user->getfirstName();
-        $this->email = $user->getemail();
-        $this->mdp = $user->getmdp();
-        $this->dateNaissance = $user->getdateNaissance();
-        $this->numTel = $user->getnumTel();
-        //$SQL = $this->connexion->prepare("INSERT INTO CLIENTS (Nom,Prenom,DateNaissance,NumeroTel,Mail,MDP) VALUES ($this->name,$this->firstName,$this->dateNaissance,$this->numTel,$this->email,$this->mdp)");
+    public function addNewUser(){
         $SQL = $this->connexion->prepare("INSERT INTO CLIENTS (Nom,Prenom,DateNaissance,NumeroTel,Mail,MDP) VALUES (:name1,:firstName,:dateNaissance,:numTel,:email,:mdp)");
         $requeteTest = $SQL->execute(array("name1"=>$this->name,"firstName"=>$this->firstName,"dateNaissance"=>$this->dateNaissance,"numTel"=>$this->numTel,"email"=>$this->email,"mdp"=>$this->mdp));
-        
-
         if($requeteTest){
             echo"compte crée";
         }
@@ -53,6 +44,75 @@ class Clients
         ORDER BY achat.IDAchat DESC
         LIMIT 10;")->fetchall();
         return $SQL;
+    }
+
+    public function getInformation($id){
+        
+    }
+
+
+
+
+
+
+    public function setUser($name,$firstName,$dateNaissance,$numTel,$email,$mdp)
+    {
+        $this->name = $name;
+        $this->firstName = $firstName;
+        $this->dateNaissance = $dateNaissance;
+        $this->numTel = $numTel;
+        $this->email = $email;
+        $this->mdp = $mdp;
+        
+    }
+
+    public function getname() {
+        return $this->name;
+    }
+    public function setname( $Name) {
+        $this->name = $Name;
+    }
+    public function getfirstName() {
+        return $this->firstName;
+    }
+    public function setfirstName( $FirstName) {
+        $this->firstName = $FirstName;
+    }
+    public function getdateNaissance() {
+        return $this->dateNaissance;
+    }
+    public function setdateNaissance( $DateNaissance) {
+        $this->dateNaissance = $DateNaissance;
+    }
+    public function getVille() {
+        return $this->Ville;
+    }
+    public function setVille( $ville) {
+        $this->Ville = $ville;
+    }
+    public function getCodePostal() {
+        return $this->CodePostal;
+    }
+    public function setCodePostal( $codePostal) {
+        $this->CodePostal = $codePostal;
+    }
+    public function getnumTel() {
+        return $this->numTel;
+    }
+    public function setnumTel( $NumTel) {
+        $this->numTel = $NumTel;
+    }
+    public function getemail() {
+        return $this->email;
+    }
+    public function setemail( $email) {
+        $this->email = $email;
+    }
+    public function getmdp() {
+        return $this->mdp;
+    }
+    public function setmdp( $mdp) {
+        $this->mdp = $mdp;
     }
 }
 
